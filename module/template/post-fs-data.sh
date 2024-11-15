a239d24ff717e46b015b5a492927b968b226fc5a
Memory <134070804+Memory2314@users.noreply.github.com>
2024-11-15 09:13:58 +0800

    Check Shamiko module and add missing GMS proc
    from chiteroman/PlayIntegrityFix

--- module/template/post-fs-data.sh
+++ module/template/post-fs-data.sh
@@ -1,1 +1,12 @@
 MODDIR=${0%/*}
+
+# Remove Play Services from Magisk DenyList when set to Enforce in normal mode
+if magisk --denylist status; then
+    magisk --denylist rm com.google.android.gms
+else
+    # Check if Shamiko is installed and whitelist feature isn't enabled
+    if [ -d "/data/adb/modules/zygisk_shamiko" ] && [ ! -f "/data/adb/shamiko/whitelist" ]; then
+        magisk --denylist add com.google.android.gms com.google.android.gms
+        magisk --denylist add com.google.android.gms com.google.android.gms.unstable
+    fi
+fi
